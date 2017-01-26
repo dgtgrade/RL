88ff7e5a24fef649dddef291daae5794b649f721
sedong <dgtgrade@gmail.com>
2017-01-26 14:11:36 +0900

    Gym 기준으로 Solved 되는 상태

--- 1002.py
+++ 1002.py
@@ -1,11 +1,10 @@
-# Random search idea from:
+# Follows a random search idea from:
 # http://kvfrans.com/simple-algoritms-for-solving-cartpole/
 
-import gym
+# import gym
 import gym.wrappers
 import numpy as np
 import time
-import sys
 
 #
 float_formatter = lambda x: "%.6f" % x
@@ -16,23 +15,29 @@ np.set_printoptions(formatter={'float_kind': float_formatter})
 #
 env = gym.make('CartPole-v0')
 env = gym.wrappers.Monitor(env, 'tmp/cartpole-experiment-1', force=True)
+
 #
+EP_MAX = 1000
 T_MAX = 200
+#
+# Solved Requirements:
+# Considered solved when the average reward is
+# greater than or equal to 195.0 over 100 consecutive trials.
+CONSECUTIVE_EPS_TO_SOLVE = 100
+T_TO_SOLVE = 195
 
 #
 t_best = 0
+params_best = np.zeros(4)
+consecutive_success = 0
 
-for ep in range(200):
+for ep in range(EP_MAX):
 
+    # noinspection PyRedeclaration
     observation = env.reset()
 
-    # params = np.random.random(4) * 2 - 1
-
-    # params found with this algorithm which sometimes ran 200 timesteps
-    # params = np.array([0.134581, -0.831711, 0.888523, 0.221410])
-
-    # params found with this algorithm which always(?) ran 200 timesteps
-    params = np.array([-0.043532, 0.372516, 0.324205, 0.902193])
+    if consecutive_success == 0:
+        params = np.random.random(4) * 2 - 1
 
     print("ep: {}".format(ep))
     print("params: {}".format(params))
@@ -47,6 +52,8 @@ for ep in range(200):
 
         # print("observation: {}".format(observation))
         action = 1 if np.dot(observation, params) > 0 else 0
+
+        #
         observation, reward, done, info = env.step(action)
 
         if done:
@@ -59,5 +66,14 @@ for ep in range(200):
             t_best))
         t_best = t
 
-    if t == T_MAX:
+    if t > T_TO_SOLVE:
+        consecutive_success += 1
+    else:
+        consecutive_success = 0
+
+    print("Consecutive Successes: {}".format(consecutive_success))
+
+    if consecutive_success == CONSECUTIVE_EPS_TO_SOLVE:
+        print("WOW! {} consecutive success! problem solved".format(
+            consecutive_success))
         break
